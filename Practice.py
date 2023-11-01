bfe3cf6f983c47cf333ea0a1a56d528e89823d12
Tyrell <tyrellcarter256@gmail.com>
2023-11-01 17:08:30 -0400

    Intial Commit

---
+++ Practice.py
@@ -0,0 +1,18 @@
+def Odd_or_Even(num):
+    if num % 2 == 0:
+        print("Even")
+    else:
+        print("Odd")
+Odd_or_Even(100)
+Odd_or_Even(78)
+
+def Pass_or_Fail(num):
+    if num >= 50:
+        print("Pass")
+    else: 
+        print("Fail")
+Pass_or_Fail(4)
+Pass_or_Fail(27)
+
+
+
