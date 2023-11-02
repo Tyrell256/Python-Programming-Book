06dc1b229224851fd92444a47ec1336563339b90
Tyrell <tyrellcarter256@gmail.com>
2023-11-02 13:44:24 -0400

    Ch 8

--- Practice.py
+++ Practice.py
@@ -14,5 +14,26 @@ def Pass_or_Fail(num):
 Pass_or_Fail(4)
 Pass_or_Fail(27)
 
+# Chapter 8: Loops in Python
 
+numbers = [12, 3, 18, 10, 7, 2, 3, 6, 1] #Variable name storing the list
+sum = 0
+for i in numbers:
+    sum = sum + i
+print("The sum is" , sum)
+
+
+def add(list):
+    sum = 0
+    for i in list:
+        sum = sum + i
+    print("The sum is" , sum)
+
+marks=[3,8,19,6,18,29,15]
+ages=[12,17,14,18,11,10,16]
+mileage=[15,67,89,123,76,83]
+cups=[7,10,3,5,8,16,13]
+
+
+add(marks)
 
