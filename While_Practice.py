7530d0b0b330b5f8228107b9f1fc235bc4ea5993
Tyrell <tyrellcarter256@gmail.com>
2023-11-05 14:23:49 -0500

    Added For and While loops

---
+++ While_Practice.py
@@ -0,0 +1,19 @@
+#While Loop Practice
+'''track = 0
+while track<4:
+    print("Within the loop")
+    track = track + 1
+else:
+    print("Now within the else segment")'''
+    
+for tracker in "bring":
+    if tracker == "i":
+        break
+    print(tracker)
+print("The End")
+
+for tracker in "bring":
+    if tracker == "i":
+        continue
+    print(tracker)
+print("finished")
